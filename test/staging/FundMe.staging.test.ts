058c749bac0a2fee86e65f5aec66001f737e6a90
Tiago Tavares <bigtavadev@gmail.com>
2023-04-11 23:52:56 +0100

    tests: sepolia staging tests

---
+++ test/staging/FundMe.staging.test.ts
@@ -0,0 +1,46 @@
+import { assert } from "chai"
+import { ethers, network } from "hardhat"
+import { developmentChains } from "../../helper-hardhat-config"
+import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers"
+import { FundMe } from "../../typechain-types"
+import { ContractTransaction } from "ethers"
+
+developmentChains.includes(network.name)
+    ? describe.skip
+    : describe("FundMe Staging Tests", async function () {
+          let fundMe: FundMe
+          let deployer: SignerWithAddress
+
+          const sendValue = ethers.utils.parseEther("0.1")
+          beforeEach(async function () {
+              const accounts = await ethers.getSigners()
+              deployer = accounts[0]
+              fundMe = await ethers.getContract("FundMe", deployer.address)
+          })
+
+          it("Allows people to fund and withdraw", async function () {
+              // Arrange
+              let transactionResponse: ContractTransaction
+
+              // Act
+              transactionResponse = await fundMe.fund({
+                  value: sendValue,
+              })
+              await transactionResponse.wait()
+
+              transactionResponse = await fundMe.withdraw({
+                  gasLimit: 100000,
+              })
+              await transactionResponse.wait()
+              const endingFundMeBalance = await fundMe.provider.getBalance(
+                  fundMe.address
+              )
+              console.log(
+                  endingFundMeBalance.toString() +
+                      " should equal 0, running assert equal..."
+              )
+
+              // Assert
+              assert.equal(endingFundMeBalance.toString(), "0")
+          })
+      })
